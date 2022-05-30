b217d623aa32f993a9cfdfe75795cb4c5a305018
ddanny16 <dammydan16@gmail.com>
2022-05-30 13:52:03 +0200

    ...

--- script.js
+++ script.js
@@ -7,25 +7,34 @@ function checkPassword() {
   if (password.length != 0) {
     if (password == cnfrmPassword) {
       message.textContent = "Passwords match";
-      message.style.backgroundColor ="green"
-      message.style.borderRadius ="10px"
+      message.style.backgroundColor = "green";
+      message.style.borderRadius = "10px";
+      let location = "/index2.html";
+
+      window.location.assign(location);
+      
+    } else if (cnfrmPassword == "") {
+      message.textContent = "confirm Password cant be empty";
+      message.style.backgroundColor = "yellow";
     }
-    else{
-        message.textContent = "Password don't match";
-        
-        message.style.backgroundColor ="red"
-        message.style.borderRadius ="10px"
+
+    // else if(cnfrmPassword==""){
+    //   message.textContent = "Password1 cant be empty";
+    //   message.style.backgroundColor ="brown"
+    // }
+    else {
+      message.textContent = "Password don't match";
+
+      message.style.backgroundColor = "red";
+      message.style.borderRadius = "10px";
     }
-  } 
-  else{
+  } else {
     //   alert('password cant be empty')
     //   message.textContent = ""
 
     message.textContent = "password cant be empty";
-        
-        message.style.backgroundColor ="blue"
-        message.style.borderRadius ="10px"
 
-
+    message.style.backgroundColor = "blue";
+    message.style.borderRadius = "10px";
   }
 }
